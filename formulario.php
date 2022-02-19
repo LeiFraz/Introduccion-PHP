fc71a0ec79f563c3be6ff537ddd2f65e7157de42
LeiFraz <flajarkin99@hotmail.com>
2022-02-19 20:23:14 -0300

    De momento solo uso css

---
+++ formulario.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduccion PHP</title>
+    <link rel="stylesheet" href="cssPropio/normalizacion.css">
+    <link rel="stylesheet" href="cssPropio/estiloIndex.css">
+    <link rel="stylesheet" href="cssPropio/estiloFormulario.css">
+
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+    <nav>
+        <a href="index.php">Inicio</a> <!--inicio de la pagina-->
+        <a href="formulario.php">Formulario</a> <!--formulario de la pagina-->
+        <a href="">Datos</a>   <!--Se muestran los datos cargados en la pagina-->
+        <a href="">Procesos</a> <!--Pensando-->
+    </nav>
+
+    <section class="miFormulario">
+        Mi Formulario
+        <hr>
+        <form class="formu" id="formu" name="formu" action="" method="POST">
+
+            <!-- GRUPO USUARIO -->
+            <div class="formu__grupo" id="grupo__usuario">
+                <label for="usuario" class="formu__label"> Usuario</label>
+
+                <div class="formu__grupo-input">
+                    <input class="formu__input" type="text" id="usuario" name="usuario" placeholder="Ingresa tu usuario">
+                    <i class="formu__validacion-estado fa fa-thumbs-up"></i>
+                    <i class="formu__validacion-estado fa fa-thumbs-down"></i>
+                </div>
+
+                <p class="formu__input-error">El usuario tiene que ser de 4 a 16 digitos y solo puede contener numeros, letras y guion bajo.</p>
+            </div>
+
+            <!-- GRUPO NOMBRE -->
+            <div class="formu__grupo" id="grupo__nombre">
+
+                <label for="nombre" class="formu__label"> Nombre</label>
+
+                <div class="formu__grupo-input">
+                    <input class="formu__input" type="text" id="nombre" name="nombre" placeholder="Ingresa tu nombre">
+                    <i class="formu__validacion-estado fa fa-thumbs-up"></i>
+                    <i class="formu__validacion-estado fa fa-thumbs-down"></i>
+                </div>
+
+                <p class="formu__input-error">El nombre tiene que ser de 4 a 16 digitos y solo debe contener letras.</p>
+            </div>
+
+            <!-- GRUPO APELLIDO -->
+            <div class="formu__grupo" id="grupo__apellido">
+
+                <label for="apellido" class="formu__label"> Apellido</label>
+
+                <div class="formu__grupo-input">
+                    <input class="formu__input" type="text" id="apellido" name="apellido" placeholder="Ingresa tu apellido">
+                    <i class="formu__validacion-estado fa-regular fa-thumbs-up"></i>
+                    <i class="formu__validacion-estado fa-regular fa-thumbs-down"></i>
+                </div>
+
+                <p class="formu__input-error">El apellido tiene que ser de 4 a 16 digitos y solo debe contener letras.</p>
+            </div>
+
+            <!-- GRUPO CORREO -->
+            <div class="formu__grupo" id="grupo__correo">
+
+                <label for="correo" class="formu__label"> Correo</label>
+
+                <div class="formu__grupo-input">
+                    <input class="formu__input" type="email" id="correo" name="correo" placeholder="Ingresa tu correo">
+                    <i class="formu__validacion-estado fa-regular fa-thumbs-up"></i>
+                    <i class="formu__validacion-estado fa-regular fa-thumbs-down"></i>
+                </div>
+
+                <p class="formu__input-error">El correo solo puede contener letras, numeros, guiones, guion bajo y puntos.</p>
+            </div>
+
+            <!-- GRUPO MENSAJE ERROR -->
+            <div class="formu__mensaje-error" id="formu_mensaje-error">
+                <p><i class="fas fa-exclamation-triangle"></i> <b>ERROR:</b> Complete todos los campos correctamente.</p>
+            </div>
+
+            <!-- GRUPO BOTON -->
+            <div class="formu__grupo formu__grupo-btn-enviar">
+                <button class="formu__btn" type="submit"> Enviar</button>
+                <p class="formu__mensaje-exito">Formulario enviado exitosamente!</p>
+            </div>
+        </form>
+    </section>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduccion PHP</title>
+    <link rel="stylesheet" href="cssPropio/normalizacion.css">
+    <link rel="stylesheet" href="cssPropio/estiloIndex.css">
+</head>
+<body>
+    <nav>
+        <a href="index.php">Inicio</a> <!--inicio de la pagina-->
+        <a href="formulario.php">Formulario</a> <!--formulario de la pagina-->
+        <a href="">Datos</a>   <!--Se muestran los datos cargados en la pagina-->
+        <a href="">Procesos</a> <!--Pensando-->
+    </nav>
+
+    <div class="contenedorPrincipal">
+        <section class="seccion">
+            <div class="card">
+                <div class="parrafo">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt dictum augue quis eleifend. Vivamus convallis ipsum ut fringilla vestibulum. Fusce urna odio, elementum ut condimentum eu, ultricies non arcu. Nam eget ligula eget nisl luctus semper at ut arcu. Suspendisse dictum ipsum eget leo varius, in tincidunt nulla convallis. Sed et purus ut justo auctor sagittis. Vestibulum tempus risus a leo tristique varius. Aliquam vel nibh at augue aliquam auctor sed nec libero. Fusce pulvinar lacus eget dictum tempor. Ut eget dictum enim, non fermentum lorem. Sed rhoncus libero in pretium facilisis. Vestibulum ut risus vitae nibh dignissim congue eget sit amet nisl. Nulla id felis dolor.
+                        Cras facilisis dui sed arcu ullamcorper consequat. Duis vel leo imperdiet, vestibulum nisi id, malesuada dui. In tincidunt fringilla ex, vel placerat nisi aliquam in. Phasellus ac venenatis magna, vitae congue elit. Aenean augue magna, accumsan finibus orci a, viverra vehicula elit. Maecenas euismod, nibh in sagittis ornare, nulla felis porta lacus, nec consectetur mi nibh id felis. Nam vulputate enim vel maximus tempor. Sed nibh tortor, aliquet et maximus sit amet, pretium vitae velit. Aliquam luctus ullamcorper fringilla. Quisque condimentum purus ut ipsum euismod, quis maximus odio vestibulum. Pellentesque eget consectetur mi, vel mattis lorem. Vivamus a lacinia lorem. Quisque sit amet odio non est varius varius at sit amet mi.
+                        Donec laoreet justo et sollicitudin euismod. Etiam tincidunt malesuada congue. Suspendisse potenti. Sed pulvinar interdum velit, a egestas magna semper nec. Vestibulum quis rutrum turpis. Praesent pretium, orci ut cursus lacinia, ipsum magna lobortis nunc, et pharetra ipsum lacus in urna. Aliquam sodales pellentesque convallis. Cras ut imperdiet est, eu vulputate lectus. Ut ut eros mi. Pellentesque facilisis, ligula vitae lacinia laoreet, leo orci tincidunt leo, nec sollicitudin metus neque auctor mi. Donec at scelerisque ante. Etiam sagittis venenatis augue, vitae laoreet nisi volutpat sed. Maecenas blandit convallis pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                        Quisque a est vel magna congue tempor in nec risus. Suspendisse est orci, congue eu tincidunt non, vehicula ut risus. Nunc sit amet ante eu velit consectetur dictum at et velit. Morbi id consequat dolor. Nunc nec tempor risus. Curabitur maximus est pulvinar molestie rutrum. Donec pharetra lorem non metus congue, ac commodo justo sagittis. Nullam convallis non erat ut mollis. Pellentesque pharetra mollis magna, sit amet ornare sem cursus in. Integer augue leo, placerat sed tristique porta, pulvinar auctor odio. Cras rhoncus dictum fermentum. Sed velit ex, vestibulum a volutpat vel, ultrices sodales felis. Suspendisse ultricies lobortis turpis, eu tincidunt nulla scelerisque sit amet. Nulla fermentum volutpat leo, viverra gravida justo convallis eget.
+                        Praesent suscipit nulla sit amet gravida posuere. Proin nunc dolor, tincidunt eget sollicitudin non, molestie malesuada ipsum. Donec vel mattis lacus, eget posuere elit. Donec posuere nulla urna, at tincidunt dolor egestas ac. Quisque sagittis mi eu commodo aliquet. Etiam scelerisque enim elit. Suspendisse sed aliquam ligula, non viverra sapien. Nunc dignissim sodales nisl laoreet tempor. Sed ultricies tortor placerat ligula molestie, id viverra lorem fermentum.
+                    </p>
+                </div>
+
+                <div class="imagen">
+                    <img src="imagenes/php.jpg" alt="Logo de PHP">
+                </div>
+            </div>
+        </section>
+    </div>
+
+    
+</body>
+</html>
